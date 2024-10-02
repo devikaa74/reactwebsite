c6b12f6fb1d5847d700047807129aea764506103
devikaa74 <devikarajendran74@gmail.com>
2024-10-02 22:21:30 +0530

    websiteReact v1

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,52 +1,56 @@
-import React from 'react'
-
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div style={{height:'300px'}} className='container mt-5 w-100'>
-      <div className="d-flex justify-content-between">
-        <div style={{width:'400px'}} className="intro">
+    <div style={{height:'auto'}} className='container mt-5 w-100'>
+      <div className="row">
+        {/* Intro Section */}
+        <div className="col-12 col-md-6 col-lg-4 mb-4">
           <h5 className='fw-bolder text-dark'>LIP<span className='text-warning'>STICK</span></h5>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque ratione asperiores quisquam obcaecati nam dolorum a laborum! Repellat quasi illum atque? Quas, minus! Impedit facere ea eos iure, sunt error!</p>
           <p>Code licenced Lipstick, docs CC by 3.0.</p>
           <p>Currently v5.3.2.</p>
         </div>
-        <div className="d-flex flex-column">
+        
+        {/* Links Section */}
+        <div className="col-12 col-md-6 col-lg-2 mb-4">
           <h5>Links</h5>
-          <a href="#" style={{textDecoration:'none', color:'white'}} target='_blank'>Home</a>
-            <a href="#" style={{textDecoration:'none', color:'white'}} target='_blank'>Products</a>
-            <a href="#" style={{textDecoration:'none', color:'white'}} target='_blank'>Shop</a>
-            <a href="#" style={{textDecoration:'none', color:'white'}} target='_blank'>Login</a>
+          <a href="#" className='text-decoration-none text-white d-block'>Home</a>
+          <a href="#" className='text-decoration-none text-white d-block'>Products</a>
+          <a href="#" className='text-decoration-none text-white d-block'>Shop</a>
+          <a href="#" className='text-decoration-none text-white d-block'>Login</a>
+        </div>
 
-        </div>
-        <div className="d-flex flex-column">
+        {/* Guides Section */}
+        <div className="col-12 col-md-6 col-lg-2 mb-4">
           <h5>Guides</h5>
-            <a href="#" style={{textDecoration:'none', color:'white'}} target='_blank'>React</a>
-            <a href="#" style={{textDecoration:'none', color:'white'}} target='_blank'>React Bootstrap</a>
+          <a href="#" className='text-decoration-none text-white d-block'>React</a>
+          <a href="#" className='text-decoration-none text-white d-block'>React Bootstrap</a>
         </div>
-        <div className="d-flex flex-column">
+
+        {/* Contact Us Section */}
+        <div className="col-12 col-md-6 col-lg-4 mb-4">
           <h5>Contact Us</h5>
           <div className="d-flex">
             <input placeholder='Enter your email here' type="text" className='form-control' />
-            <button className='btn btn-info ms-2'> <i className='fa-solid fa-arrow-right'></i> </button>
+            <button className='btn btn-info ms-2'> 
+              <i className='fa-solid fa-arrow-right'></i> 
+            </button>
           </div>
           <div className="icons d-flex justify-content-between mt-3">
-        <a href="" style={{textDecoration:'none', color:'white'}} target='_blank'><i className='fa-brands fa-twitter'></i> </a>
-        <a href="" style={{textDecoration:'none', color:'white'}} target='_blank'><i className='fa-brands fa-instagram'></i> </a>
-        <a href="" style={{textDecoration:'none', color:'white'}} target='_blank'><i className='fa-brands fa-facebook'></i> </a>
-        <a href="" style={{textDecoration:'none', color:'white'}} target='_blank'><i className='fa-brands fa-linkedin'></i> </a>
-        <a href="" style={{textDecoration:'none', color:'white'}} target='_blank'><i className='fa-brands fa-github'></i> </a>
-        <a href="" style={{textDecoration:'none', color:'white'}} target='_blank'><i className='fa-solid fa-phone'></i> </a>
-
-
+            <a href="#" className='text-decoration-none text-white' target='_blank'><i className='fa-brands fa-twitter'></i></a>
+            <a href="#" className='text-decoration-none text-white' target='_blank'><i className='fa-brands fa-instagram'></i></a>
+            <a href="#" className='text-decoration-none text-white' target='_blank'><i className='fa-brands fa-facebook'></i></a>
+            <a href="#" className='text-decoration-none text-white' target='_blank'><i className='fa-brands fa-linkedin'></i></a>
+            <a href="#" className='text-decoration-none text-white' target='_blank'><i className='fa-brands fa-github'></i></a>
+            <a href="#" className='text-decoration-none text-white' target='_blank'><i className='fa-solid fa-phone'></i></a>
+          </div>
         </div>
-        </div>
-       
       </div>
-      <p className="text-center mt-3">Copyright @copy; All world brand, Lipstick.
-        </p>
+      
+      <p className="text-center mt-3">Copyright &copy; All world brand, Lipstick.</p>
     </div>
   )
 }
 
-export default Footer
+export default Footer;
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,32 +1,18 @@
-import React from 'react'
+import React from 'react';
 import Card from 'react-bootstrap/Card';
-import imgR2 from './assets/imgr2.png'
-import imgR3 from './assets/imgr3.png'
-import imgR4 from './assets/imgr4.png'
-import imgR5 from './assets/imgr5.png'
-
+import imgR2 from './assets/imgr2.png';
+import imgR3 from './assets/imgr3.png';
+import imgR4 from './assets/imgr4.png';
+import imgR5 from './assets/imgr5.png';
 
 const Products = () => {
   return (
     <div>
-        {/* <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-          
-        </Card>
-         */}
-     <div className="my-5">
+      <div className="my-5">
         <h3 className='text-center'>Our Best Sellers*</h3>
-        <div className="row mt-5">
-          <div className="col-lg-3">
-            <Card style={{ width: '20rem' }}>
+        <div className="row mt-5 g-4"> {/* Add gap between cards with g-4 */}
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <Card style={{ width: '100%' }}> {/* Set width to 100% to take full space */}
               <Card.Img style={{ width: '100%', height: '200px', objectFit: 'cover' }} variant="top" src={imgR2} />
               <Card.Body>
                 <Card.Text>
@@ -34,12 +20,11 @@ const Products = () => {
                   <p style={{color:'red'}}><span className='fw-bolder'>$43.20</span> with code</p>
                 </Card.Text>
               </Card.Body>
-
             </Card>
-
           </div>
-          <div className="col-lg-3">
-            <Card style={{ width: '20rem' }}>
+
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <Card style={{ width: '100%' }}>
               <Card.Img style={{ width: '100%', height: '200px', objectFit: 'cover' }} variant="top" src={imgR3} />
               <Card.Body>
                 <Card.Text>
@@ -47,12 +32,11 @@ const Products = () => {
                   <p style={{color:'red'}}><span className='fw-bolder'>$43.20</span> with code</p>
                 </Card.Text>
               </Card.Body>
-
             </Card>
-
           </div>
-          <div className="col-lg-3">
-            <Card style={{ width: '20rem' }}>
+
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <Card style={{ width: '100%' }}>
               <Card.Img style={{ width: '100%', height: '200px', objectFit: 'cover' }} variant="top" src={imgR4} />
               <Card.Body>
                 <Card.Text>
@@ -60,12 +44,11 @@ const Products = () => {
                   <p style={{color:'red'}}><span className='fw-bolder'>$43.20</span> with code</p>
                 </Card.Text>
               </Card.Body>
-
             </Card>
-
           </div>
-          <div className="col-lg-3">
-            <Card style={{ width: '20rem' }}>
+
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <Card style={{ width: '100%' }}>
               <Card.Img style={{ width: '100%', height: '200px', objectFit: 'cover' }} variant="top" src={imgR5} />
               <Card.Body>
                 <Card.Text>
@@ -73,17 +56,12 @@ const Products = () => {
                   <p style={{color:'red'}}><span className='fw-bolder'>$43.20</span> with code</p>
                 </Card.Text>
               </Card.Body>
-
             </Card>
-
           </div>
-
         </div>
       </div>
-
-
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
